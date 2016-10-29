e58c9c63db7cb0e9bb63d489e843c00287d3b642
Tom Donoghue <tdonoghue@ucsd.edu>
2016-10-29 14:21:05 -0700

    Add outlines for count and word tests

---
+++ erpsc/tests/test_count.py
@@ -0,0 +1,10 @@
+from erpsc.count import *
+
+#########################################
+#### TESTS - ERPSC - COUNT - CLASSES ####
+#########################################
+
+def test_erpsc_count():
+    """   """
+
+    assert ERPSCCount()
---
+++ erpsc/tests/test_words.py
@@ -0,0 +1,10 @@
+from erpsc.words import *
+
+#########################################
+#### TESTS - ERPSC - WORDS - CLASSES ####
+#########################################
+
+def test_erpsc_words():
+    """   """
+
+    assert ERPSCWords()
