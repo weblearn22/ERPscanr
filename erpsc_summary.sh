6691be53d555789f98ab69149b174f59c851a1c8
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-06 11:02:09 -0800

    ENH: Add coverage check for tests. Also, space out output

--- erpsc_summary.sh
+++ erpsc_summary.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 # Check how many lines there are in module - excludes blank lines
-printf "\nNumber of lines of code & comments in OM: "
+printf "\n\n\n\nNumber of lines of code & comments in OM: "
 find ./erpsc -name "*.py" -type f -exec grep . {} \; | wc -l
-printf "\n"
 
 # Check number of files using cloc
-printf "\n CLOC OUTPUT: \n"
+printf "\n\n\n CLOC OUTPUT: \n"
 cloc erpsc
 
-# Run Tests
-printf "\n RUN TESTS: \n"
-py.test
+# Run Tests & Check Coverage
+printf "\n\n\n RUN TESTS: \n"
+coverage run --source erpsc -m py.test
+coverage report
+#py.test
 
 # Find a way to get summary from pylint?
+
+# Print out some new lines
+printf "\n\n\n\n"
