a6230d510c6aed6abe37ec1e7589aa52f29fbf52
Tom Donoghue <tdonoghue@ucsd.edu>
2016-11-29 15:32:03 -0800

    Add tests for check functions

--- erpsc/tests/test_base.py
+++ erpsc/tests/test_base.py
@@ -16,7 +16,7 @@ def test_erpsc_base():
     assert Base()
 
 def test_set_erps():
-    """Test the set_erps method of ERPSCBase."""
+    """Test the set_erps method of Base."""
 
     base = Base()
     base.set_erps(['N100', 'P100'])
@@ -24,12 +24,22 @@ def test_set_erps():
     assert base.erps
 
 def test_set_erps_file():
-    """Test the set_erps_file method of ERPSCBase."""
+    """Test the set_erps_file method of Base."""
 
     base = Base()
     base.set_erps_file()
 
     assert base.erps
+
+def tests_check_erps():
+    """   """
+
+    base = Base()
+    base.set_erps_file()
+
+    base.check_erps()
+
+    assert True
 
 def test_unload_erps():
     """Test unloading of ERP words."""
@@ -43,7 +53,7 @@ def test_unload_erps():
     assert not base.n_erps
 
 def test_set_exclusions():
-    """Test the set_exclusions method of ERPSCBase."""
+    """Test the set_exclusions method of Base."""
 
     base = Base()
     base.set_erps(['N100', 'P100'])
@@ -52,13 +62,24 @@ def test_set_exclusions():
     assert base.exclusions
 
 def test_set_exclusions_file():
-    """Test the set_exclusions_file method of ERPSCBase."""
+    """Test the set_exclusions_file method of Base."""
 
     base = Base()
     base.set_erps_file()
     base.set_exclusions_file()
 
     assert base.exclusions
+
+def test_check_exclusions():
+    """   """
+
+    base = Base()
+    base.set_erps_file()
+    base.set_exclusions_file()
+
+    base.check_exclusions()
+
+    assert True
 
 def test_unload_exclusions():
     """Test unloading of exclusion words."""
@@ -72,7 +93,7 @@ def test_unload_exclusions():
     assert not base.exclusions
 
 def test_set_terms():
-    """Test the set_terms method of ERPSCBase."""
+    """Test the set_terms method of Base."""
 
     base = Base()
     base.set_terms(['think', 'do'])
@@ -80,7 +101,7 @@ def test_set_terms():
     assert base.terms
 
 def test_set_terms_file_cog():
-    """Test the set_terms_file method of ERPSCBase, for cognitive files."""
+    """Test the set_terms_file method of Base, for cognitive files."""
 
     base = Base()
     base.set_terms_file('cognitive')
@@ -88,12 +109,23 @@ def test_set_terms_file_cog():
     assert base.terms
 
 def test_set_terms_file_dis():
-    """Test the set_terms_file method of ERPSCBase, for disease files."""
+    """Test the set_terms_file method of Base, for disease files."""
 
     base = Base()
     base.set_terms_file('disease')
 
     assert base.terms
+
+def test_check_terms():
+    """   """
+
+    base = Base()
+
+    base.set_terms_file('cognitive')
+
+    base.check_terms()
+
+    assert True
 
 def test_unload_terms():
     """Test unloading of term words."""
