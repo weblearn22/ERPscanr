d6d3b3eff5716c45a8d3e08b00fdc6e6a16e2139
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-20 16:20:11 -0500

    Reorganize how URLs object works

--- erpsc/core/urls.py
+++ erpsc/core/urls.py
@@ -19,8 +19,8 @@ class URLS(object):
 
     Attributes
     ----------
-    lit_db : ?
-        xx
+    lit_db : str
+        The literature database to use.
     eutils : str
         Base URL for the e-utils tools.
     search : str
@@ -51,19 +51,17 @@ class URLS(object):
         self.search = str()
         self.fetch = str()
 
-        # TODO: Save settings in a dictionary?
+        # Initialize dictionary to save settings, and add settings to it
         self.settings = dict()
+        self.save_settings(lit_db, str(retmax_val), field_val, retmode_val)
 
-        # Save settings to object
-        self.lit_db = lit_db
-        self.retmax_val = retmax_val
-        self.field_val = field_val
-        self.retmode_val = retmode_val
+        # Initialize dictionary to save url arguments, and populate it from settings
+        self.args = dict()
+        self.save_args()
 
         if auto_gen:
-            self.build_param_settings
-            self.build_search_url
-            self.build_fetch_url
+            self.build_search_url(['db', 'retmode'])
+            self.build_fetch_url(['db', 'retmode'])
 
         # OLD:
         # Set the search url
@@ -71,25 +69,73 @@ class URLS(object):
         #self.fetch = self.base_url + ''
         #self.retmax = ''
 
-    def build_param_settings(self):
+
+    def save_settings(self, db=None, retmax=None, field=None, retmode=None):
+        """
+
+        Parameters
+        ----------
+
+        Notes
+        -----
+        - All possible settings are set as possible arguments to this function. For each of
+            these possible settings, all of which are given a value are saved out into the dictionary.
+        - The 'locals()' function returns a dictionary of variables in scope (in this function).
+        - Using 'locals()' saves separately defining a list of possible variables, that
+            would need to be maintained to make sure it matched the method arguments.
+
+        Equivalent and more explicit:
+
+        # Initialize list of possible settings, and remove the self argument
+        possible_settings = locals().keys()
+        possible_settings.remove('self')
+
+        # Loop through all possible settings
+        for ps in possible_settings:
+
+            # If defined (not None) set the value of the setting
+            #   into a dictionary, with key of the name of the setting
+            if eval(ps):
+                self.settings[ps] = eval(ps)
+        """
+
+        # Save all defined settings to a dictionary
+        self.settings = {k: v for k, v in locals().items() if k is not 'self' and v is not None}
+
+
+    def save_args(self):
         """   """
 
-        # Parameter
-        self.db_arg = 'db=' + self.lit_db
-        self.retmax_arg = 'retmax=' + str(self.retmax_val)
-        self.field_arg = 'field=' + self.field_val
-        self.retmode_arg = 'retmode=' + self.retmode_val
+        # For each parameter in settings, create the url argument
+        for param in self.settings.keys():
+            self.args[param] = param + '=' + self.settings[param]
 
-    def build_search_url(self):
-        """   """
+
+    def build_search_url(self, args_to_use):
+        """
+
+        Parameters
+        ----------
+        args_to_use : ?
+            xx
+        """
 
         # Set the search url
         search_base = self.eutils + 'esearch.fcgi?'
-        self.search = search_base + db_arg + '&' + field_arg + '&' + 'term='
+        #self.search = search_base + self.db_arg + '&' + self.field_arg + '&' + 'term='
+        self.search = search_base + '&'.join([self.args[arg] for arg in args_to_use]) + '&term='
 
-    def build_fetch_url(self):
-        """   """
+
+    def build_fetch_url(self, args_to_use):
+        """
+
+        Parameters
+        ----------
+        args_to_use : ?
+            xx
+        """
 
         # Set the fetch url
         fetch_base = self.eutils + 'efetch.fcgi?'
-        self.fetch = fetch_base + db_arg + '&' + retmode_arg + '&' + 'id='
+        #self.fetch = fetch_base + self.db_arg + '&' + self.retmode_arg + '&' + 'id='
+        self.fetch = fetch_base + '&'.join([self.args[arg] for arg in args_to_use]) + '&id='
