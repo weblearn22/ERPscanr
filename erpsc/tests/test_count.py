8e3cbcb6bdd16b0e7f8a38d4401adc8881e1bd06
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-19 22:20:47 -0500

    Add outline for new tests

--- erpsc/tests/test_count.py
+++ erpsc/tests/test_count.py
@@ -1,8 +1,10 @@
+"""   """
+
 from erpsc.count import *
 
-#####################################################################
-################## TESTS - ERPSC - COUNT - CLASSES ##################
-#####################################################################
+#######################################################################################
+################################ TESTS - ERPSC - COUNT ################################
+#######################################################################################
 
 def test_erpsc_count():
     """Test the ERPSCCount object."""
@@ -10,6 +12,31 @@ def test_erpsc_count():
     # Check that ERPSCCount returns properly
     assert Count()
 
-##
-## TESTS - ERPSC - COUNT -
-##
+def test_scrape_data():
+    """   """
+
+    counts = Count()
+
+    # Add ERPs and terms
+    counts.set_erps(['N400', 'P600'])
+    counts.set_terms(['language', 'memory'])
+
+def test_check_cooc_erps():
+    """   """
+    pass
+
+def test_check_cooc_terms():
+    """   """
+    pass
+
+def test_check_top():
+    """   """
+    pass
+
+def test_check_counts():
+    """   """
+    pass
+
+def test_save_pickle():
+    """   """
+    pass
