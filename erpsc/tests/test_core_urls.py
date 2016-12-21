f4e7b99d4f7eaa5bb44ece2f53d8d0c126b69428
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-21 15:32:50 -0500

    Add proper testing of URLS

--- erpsc/tests/test_core_urls.py
+++ erpsc/tests/test_core_urls.py
@@ -1,16 +1,51 @@
-"""   """
+"""Tests for URL functions and classes from erpsc.core"""
+
+from py.test import raises
 
 from erpsc.core.urls import URLS
+from erpsc.core.errors import InconsistentDataError
 
 ##################################################################################
 ##################################################################################
 ##################################################################################
 
 def test_urls():
-    """Test the URLS object."""
+    """Test the URLS object returns properly."""
 
-    # Check that URLS returns properly, with database inputs
-    assert URLS('pubmed')
-    assert URLS('pmc')
+    assert URLS(auto_gen=False)
 
-    # NOTE: ADD ERROR CHECK - AFTER ADD ERROR CHECKING
+def test_urls_settings_args():
+    """Tests URLS() returns properly with settings provided, and args defined.
+    This triggers save_settings() and save_args() methods with inputs from __init__.
+    """
+
+    assert URLS(db='pubmed', retmax='500', field='id', retmode='xml', auto_gen=False)
+
+def test_check_args():
+    """Test the check_args() method from URLS()."""
+
+    urls = URLS(db='pubmed', field='id', auto_gen=False)
+
+    urls.check_args(['db', 'field'])
+
+    # Check error
+    with raises(InconsistentDataError):
+        urls.check_args(['db', 'retmax', 'field'])
+
+def test_build_search():
+    """Test the buid_search() method form URLS()."""
+
+    urls = URLS(db='pubmed', retmax='500', field='id', retmode='xml', auto_gen=False)
+
+    urls.build_search(['db', 'retmode'])
+
+    assert urls.search
+
+def test_build_fetch():
+    """Test the build_fetch() method from URLS()."""
+
+    urls = URLS(db='pubmed', retmax='500', field='id', retmode='xml', auto_gen=False)
+
+    urls.build_fetch(['db', 'retmode'])
+
+    assert urls.fetch
