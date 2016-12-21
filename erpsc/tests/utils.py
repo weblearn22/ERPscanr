57ac798f64103c0aee24cc95d91b671a6d9eaaa4
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-21 13:47:02 -0500

    Add helper function to load Base

--- erpsc/tests/utils.py
+++ erpsc/tests/utils.py
@@ -1,13 +1,30 @@
 """Helper functions for testing ERPSC."""
 
+from erpsc.base import Base
 from erpsc.erp_words import ERPWords
 
 ##################################################################################
 ##################################################################################
 ##################################################################################
 
+def load_base(set_erps=False, set_excl=False, set_terms=None):
+    """Helper function to load Base() object for testing."""
+
+    base = Base()
+
+    if set_erps:
+        base.set_erps_file()
+
+    if set_excl:
+        base.set_exclusions_file()
+
+    if set_terms:
+        base.set_terms_file(set_terms)
+
+    return base
+
 def load_erp_words(add_dat=False):
-    """   """
+    """Helper function to load ERPWords() object for testing."""
 
     test_erp = 'test'
     words = ERPWords(test_erp)
