0601a1afe0de695c96fcad5c1d6ba2fb411ce9d2
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-21 12:58:38 -0500

    add tests for check_db

--- erpsc/tests/test_core_db.py
+++ erpsc/tests/test_core_db.py
@@ -1,6 +1,6 @@
 """   """
 
-from erpsc.core.db import ERPDB
+from erpsc.core.db import ERPDB, check_db
 
 ##################################################################################
 ##################################################################################
@@ -11,3 +11,13 @@ def test_erpdb():
 
     # Check that ERPDB returns properly.
     assert ERPDB()
+
+def test_check_db():
+    """   """
+
+    db = check_db(None)
+    assert isinstance(db, ERPDB)
+
+    db = ERPDB()
+    db = check_db(db)
+    assert isinstance(db, ERPDB)
