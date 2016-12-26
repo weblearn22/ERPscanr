c1763887d4417a0c47327f0ad75895599dafec58
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-26 17:40:09 -0500

    Updates to summary

--- summary.sh
+++ summary.sh
@@ -6,14 +6,16 @@ printf "\nNumber of lines of code & comments in ERP-SCANR: "
 find ./erpsc -name "*.py" -type f -exec grep . {} \; | wc -l
 
 # Check number of files using cloc
-printf "\n\n\n CLOC OUTPUT: \n"
-cloc erpsc
+printf "\n\n\n CLOC OUTPUT (EXCLUDING TESTS): \n"
+cloc erpsc --exclude-dir='tests'
+
+printf "\n\n\n CLOC OUTPUT - TEST FILES: \n"
+cloc erpsc/tests --exclude-dir='data'
 
 # Run Tests & Check Coverage
 printf "\n\n\n RUN TESTS & TEST COVERAGE: \n"
-coverage run --source erpsc -m py.test
+coverage run --source erpsc --omit="*/plts/*" -m py.test
 coverage report
-#py.test
 
 # Find a way to get summary from pylint?
 
