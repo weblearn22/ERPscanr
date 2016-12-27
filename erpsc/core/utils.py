5b7b1e90cf7f4a5b8332875d3c33b060c0550cc8
Tom Donoghue <tdonoghue@ucsd.edu>
2016-12-27 12:32:29 -0500

    Add basic utilities file

---
+++ erpsc/core/utils.py
@@ -0,0 +1,32 @@
+"""Basic utility functions for ERP-SCANR."""
+
+##################################################################################
+##################################################################################
+##################################################################################
+
+def comb_terms(lst, jt):
+    """Combine a list of terms to use as search arguments.
+
+    Parameters
+    ----------
+    lst : list of str
+        List of terms to combine together.
+    jt : {'or', 'not'}
+        Term to use to join together terms.
+
+    Returns
+    -------
+    out : str
+        String
+    """
+
+    # Add quotes to list items for exact search
+    lst = ['"'+ item + '"' for item in lst]
+
+    # Join together using requested join term
+    if jt == 'or':
+        out = 'OR'.join(lst)
+    elif jt == 'not':
+        out = 'NOT' + 'NOT'.join(lst)
+
+    return out
