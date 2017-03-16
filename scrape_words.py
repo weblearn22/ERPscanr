6464c40b99f241a5881c0baaaed8300ff46f5b0a
Tom Donoghue <tdonoghue@ucsd.edu>
2017-03-16 02:10:48 -0700

    Fix up words scrape

--- scrape_words.py
+++ scrape_words.py
@@ -4,24 +4,26 @@ from erpsc.words import Words
 ###############################################################################
 
 TEST = True
+S_NAME = 'BaseScrape'
 
 ###############################################################################
 ###############################################################################
 
 def main():
-    """   """
+    """Run scrape of words data."""
 
     words = Words()
 
     if TEST:
-        counts.set_erps([['P600'], ['N170']])
-        counts.set_exclusions([['protein'], ['histone']])
+        words.set_erps([['P100'], ['P300'], ['N170'], ['P600'], ['N400']])
+        words.set_exclusions([['protein'], ['protein'], ['protein'], ['protein'], ['protein']])
     else:
-        counts.set_erps_file()
-        counts.set_exclusions_file()
+        words.set_erps_file()
+        words.set_exclusions_file()
 
-    words.scrape_data(db='pmc')
+    words.scrape_data(db='pmc', retmax=500)
 
+    save_pickle_obj(words, S_NAME)
 
 if __name__ == "__main__":
     main()
