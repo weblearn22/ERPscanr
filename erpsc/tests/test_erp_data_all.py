c24b0dc22fe5c1528669e7234161da3096a25b6e
Tom Donoghue <tdonoghue@ucsd.edu>
2017-03-19 13:19:24 -0700

    Add to tests for ERPDataAll object

--- erpsc/tests/test_erp_data_all.py
+++ erpsc/tests/test_erp_data_all.py
@@ -3,15 +3,38 @@
 from erpsc.erp_data_all import *
 from erpsc.tests.utils import load_erp_data
 
-##
-##
-##
+###################################################################################
+###################################################################################
+###################################################################################
 
 def test_erp_data_all():
-    """   """
+    """
+    Note: Constructor calls (& implicitly tests) the combine & create_freq funcs.
+    """
 
     erp_dat = load_erp_data(add_dat=True, n=2)
 
     erp_dat_all = ERPDataAll(erp_dat)
 
     assert erp_dat_all
+
+def test_check_funcs():
+    """   """
+
+    erp_dat = load_erp_data(add_dat=True, n=2)
+    erp_dat_all = ERPDataAll(erp_dat)
+
+    erp_dat_all.check_words(2)
+    erp_dat_all.check_kws(2)
+
+    assert True
+
+def test_print_summary():
+    """   """
+    pass
+    #erp_dat = load_erp_data(add_dat=True, n=2)
+    #erp_dat_all = ERPDataAll(erp_dat)
+
+    #erp_dat_all.print_summary()
+
+    #assert True
