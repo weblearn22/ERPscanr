89bac4acb9b9471b388087361b4bb32d6c587cb8
Tom Donoghue <tdonoghue@ucsd.edu>
2017-03-20 19:13:53 -0700

    Finish up prep site for words erp profiles

--- prep_site.py
+++ prep_site.py
@@ -1,14 +1,20 @@
 """   """
 
 import os
+from shutil import copyfile
 
 from erpsc.core.db import ERPDB
 from erpsc.core.db import WebDB as WDB
+
+####################################################################################
+####################################################################################
+####################################################################################
 
 def main():
     """   """
 
     db = ERPDB()
+    wdb = WDB()
 
     # Load list of labels
     with open(os.path.join(db.words_path, 'labels.txt')) as infile:
@@ -23,19 +29,31 @@ def main():
         make_post_md(label)
 
         # Website data json - copy to website directory
+        copyfile(os.path.join(db.words_path, 'summary', label + '.json'),
+                 os.path.join(wdb.dat_path, label + '.json'))
+
+        # Check website plots folder
+        w_plts_path = os.path.join(wdb.plt_path, label)
+        if not os.path.exists(w_plts_path):
+            os.mkdir(w_plts_path)
 
         # Wordcloud - copy to website directory
+        copyfile(os.path.join(db.figs_path, 'wc', label + '.png'),
+                 os.path.join(w_plts_path, 'wc.png'))
 
         # Publication graph - copy to wesbite directory
+        copyfile(os.path.join(db.figs_path, 'year', label + '.png'),
+                 os.path.join(w_plts_path, 'hist.png'))
+
 
 def make_post_md(label):
-    """   """
+    """Create the markdown post page for ERP-SCANR website."""
 
     # Get website database object
     wdb = WDB()
 
-    # Create the markdown file with yml header
-    with open(os.path.join(wdb.post_path, label + '.md'), 'w') as post_file:
+    # Create the markdown file with yml front matter
+    with open(os.path.join(wdb.post_path, '2017-03-15-' + label + '.md'), 'w') as post_file:
         post_file.write('---\n')
         post_file.write('title: \"' + label + '\"\n')
         post_file.write('date: 2017/03/15\n')
