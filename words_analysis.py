cc754415fcd373f4a5f52dc620245fef4f5a87e8
Tom Donoghue <tdonoghue@ucsd.edu>
2017-03-23 12:48:08 -0700

    Add loading functionality and status prints

--- words_analysis.py
+++ words_analysis.py
@@ -1,4 +1,4 @@
-"""   """
+"""Run analysis on scraped words data."""
 
 from erpsc.core.io import load_pickle_obj
 from erpsc.erp_data_all import ERPDataAll
@@ -16,13 +16,18 @@ F_NAME = 'BaseScrape_words'
 def main():
     """   """
 
+    # Print out status
+    print('\n\n ANALYZING WORDS DATA \n\n')
+
     # Load pickle object
     words = load_pickle_obj(F_NAME)
 
+    # Loop through all ERPs
     for erp in words.result_keys:
 
         # Check if raw data loaded - load if not
-        # TODO!
+        if not words.results[erp].n_articles:
+            words.results[erp].load()
 
         # Turn into ERPDataAll object
         erp_dat = ERPDataAll(words[erp])
@@ -38,6 +43,9 @@ def main():
         # Create and save years figure
         plot_years(erp_dat.year_counts, erp_dat.label,
                    disp_fig=False, save_fig=True)
+
+    # Print out status
+    print('\n\n ANALYZING WORDS DATA \n\n')
 
 if __name__ == "__main__":
     main()
