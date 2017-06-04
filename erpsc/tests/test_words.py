3371111ff21d908d1326034497039a3424d17ccf
Tom Donoghue <tdonoghue@ucsd.edu>
2017-06-04 15:36:28 -0400

    Update test case for changed data on test article

--- erpsc/tests/test_words.py
+++ erpsc/tests/test_words.py
@@ -65,7 +65,7 @@ def test_extract_add_info():
                                   " and the P600 in Language Processing.")
     assert erp_word.words[0][0] == "ten"
     assert erp_word.kws[0][0] == "computational modeling"
-    assert erp_word.years[0] == 2016
+    assert erp_word.years[0] == 2017
     assert erp_word.months[0] == 'Dec'
     assert erp_word.dois[0] == '10.1111/cogs.12461'
 
