1b7273030bce4d190dd3bd9141a0f40ff966f164
Tom Donoghue <tdonoghue.research@gmail.com>
2021-02-11 22:55:15 -0500

    load analysis exclusions

--- scripts/words_analysis.py
+++ scripts/words_analysis.py
@@ -2,6 +2,7 @@
 
 from lisc.data import ArticlesAll
 from lisc.utils import SCDB, load_object
+from lisc.utils.io import load_txt_file
 
 from lisc.plts.words import plot_years, plot_wordcloud
 
@@ -9,25 +10,9 @@ from lisc.plts.words import plot_years, plot_wordcloud
 ###################################################################################################
 
 # Set data words object to load
+TERM_DIR = '../terms'
 DB_NAME = '../data'
 F_NAME = 'words_erps'
-
-# Terms to exclude from word & keyword frequency distributions
-EXCLUDE_WORDS = [
-    'eeg',
-    'erp',
-    'erps',
-    'event-related'
-    'event-related potential',
-    'event-related potentials',
-    'event related potentials',
-    'event related-potentials (erps)',
-    'event-related potentials (erps)',
-    'potentials',
-    'electroencephalography',
-    'electroencephalography (eeg)',
-    'evoked'
-]
 
 # Set the year range for plotting
 YEAR_RANGE = [None, 2020]
@@ -45,6 +30,7 @@ def main():
     db = SCDB(DB_NAME)
 
     words = load_object(F_NAME, db)
+    exclusions = load_txt_file('analysis_exclusions.txt', TERM_DIR, split_elements=False)
 
     for erp in words.labels:
 
@@ -54,18 +40,18 @@ def main():
         words[erp].load(directory=db)
 
         # Aggregate data together across all articles
-        erp_data = ArticlesAll(words[erp], exclusions=EXCLUDE_WORDS)
+        erp_data = ArticlesAll(words[erp], exclusions=exclusions)
 
         # Create and save summary
         erp_data.create_summary()
         erp_data.save_summary(directory=db)
 
         # Create and save wordcloud figure
-        plot_wordcloud(erp_data.words, 20,
+        plot_wordcloud(erp_data.words, 20, transparent=True,
                        save_fig=True, f_name='wc/' + erp + PLT_EXT, directory=db, close=True)
 
         # Create and save years figure
-        plot_years(erp_data.years, year_range=YEAR_RANGE,
+        plot_years(erp_data.years, year_range=YEAR_RANGE, transparent=True,
                    save_fig=True, f_name='years/' + erp + PLT_EXT, directory=db, close=True)
 
         # Clear the loaded data for the current term
