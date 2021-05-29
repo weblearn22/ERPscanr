1ba4a114dd76efb8b446ba3cd5cf151dba25282f
Tom Donoghue <tdonoghue.research@gmail.com>
2021-05-29 15:11:47 -0400

    add network utils

--- code/analysis.py
+++ code/analysis.py
@@ -35,3 +35,17 @@ def get_time_associations(counts):
             time_associations.append(temp)
 
     return time_associations
+
+
+def scale_number(unscaled, to_min, to_max, from_min, from_max):
+    """Scale a number to be within a given range.
+    From here: http://stackoverflow.com/questions/929103
+    """
+
+    return (to_max - to_min) * (unscaled - from_min) / (from_max - from_min) + to_min
+
+
+def scale_list(lst, to_min, to_max):
+    """Scale a list of number to a given range."""
+
+    return [scale_number(ind, to_min, to_max, min(lst), max(lst)) for ind in lst]
